195a1f6f137b2cc6ca2b7f8ccf2f9e247f1558c7
U-JUSTSERGEY\Sergey <sergomorello@yandex.ru>
2021-05-31 13:23:42 +0300

    2

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 header('Content-Type: text/html; charset=utf-8');
 require_once('inc_config.php');
 require_once('inc/inc_database.php');
-//$dblink = new database();
-//$dblink->connect();
 require_once('inc/core.php');
 require_once('inc/model.php');
 require_once('inc/controller.php');
@@ -12,4 +10,3 @@ require_once('inc/router.php');
 $router = new router;
 require_once('routerConf.php');
 $router->run();
-//$dblink->disconnect();
